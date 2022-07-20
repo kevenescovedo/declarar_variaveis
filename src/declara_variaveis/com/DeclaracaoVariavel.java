193b563d62dd19623f2e6ee2d00917eaa480d33f
DESKTOP-Q1VIMM1\keven <kevenescovedo1916@gmail.com>
2022-07-20 07:47:41 -0300

    primeiro commit

---
+++ src/declara_variaveis/com/DeclaracaoVariavel.java
@@ -0,0 +1,31 @@
+package declara_variaveis.com;
+
+public class DeclaracaoVariavel {
+	
+public  static void main (String[] args) {
+	int i;
+	int I;
+	//int 1a; ocasiona em erro, pois uma variavel não pode começar com número
+	int _1a;
+	int $aq; // funciona porem não recomendado
+	int L = 10;
+   final int j = 10;
+  // int asrn246 78md; variavel não pode ser separada
+   int asrn24678_md;
+  // int asrn2$46%78_md = 10; não pode usar %
+   int  quantidadeProduto;
+   int QuantidadeProduto; //funciona porem não é romendado;
+   final int NUMERO_TENTATIVAS = 5; // VARIAVEIS FINAL CONTANTE DEVE 
+   final int   numeroTentativas;
+ //  int NUMERO_TENTATIVAS = 5; FUNCIONA MAS COMO TEM UMA VARIVEL COM ESSE NOME DÁ ERRO,
+   //PORÉM COMO DISSE UM NOME COM LETRAS MAISCULAS E _ DEVE SER USADAS PARA CONSTANTES
+   
+   
+   
+   
+   
+   
+    
+}
+
+}
